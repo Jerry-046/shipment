213c340f10c25db2fc4c8bcc31fe6ef04da7b1ad
Jerry <dare3462@gmail.com>
2024-04-18 12:18:27 +0545

    reset and feedback

--- shipping/urls.py
+++ shipping/urls.py
@@ -3,4 +3,9 @@ from shipping import views as shippingview
 
 urlpatterns = [
     path('',shippingview.index,name='shipping-home'),
+    path('feedback-create/', shippingview.feedback_create, name='feedback_create'),
+    path('<int:pk>/', shippingview.feedback_detail, name='feedback_detail'),
+    path('<int:pk>/feedback-update/', shippingview.feedback_update, name='feedback_update'),
+    path('<int:pk>/feedback-delete/', shippingview.feedback_delete, name='feedback_delete'),
+    path('feedback-list/', shippingview.feedback_list, name='feedback_list'),
 ]
--- shipping/views.py
+++ shipping/views.py
@@ -1,8 +1,48 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,get_object_or_404,redirect
+from django.contrib.auth.decorators import login_required,permission_required
+from .forms import FeedbackForm
+from .models import Feedback
 
-@login_required
 def index(request):
     return render(request,'shipping/home.html')
 
-# Create your views here.
+def feedback_list(request):
+    feedbacks = Feedback.objects.all()
+    return render(request, 'shipping/feedback_list.html', {'feedbacks': feedbacks})
+
+def feedback_detail(request, pk):
+    feedback = get_object_or_404(Feedback, pk=pk)
+    return render(request, 'shipping/feedback_detail.html', {'feedback': feedback})
+
+def feedback_create(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('feedback_list')
+    else:
+        form = FeedbackForm()
+    return render(request, 'shipping/feedback_form.html', {'form': form})
+
+@login_required
+@permission_required('feedback.delete_feedback', raise_exception=True)
+def feedback_update(request, pk):
+    feedback = get_object_or_404(Feedback, pk=pk)
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST, instance=feedback)
+        if form.is_valid():
+            form.save()
+            return redirect('feedback_list')
+    else:
+        form = FeedbackForm(instance=feedback)
+    return render(request, 'shipping/feedback_form.html', {'form': form})
+
+@login_required
+@permission_required('feedback.delete_feedback', raise_exception=True)
+def feedback_delete(request, pk):
+    feedback = get_object_or_404(Feedback, pk=pk)
+    if request.method == 'POST':
+        feedback.delete()
+        return redirect('feedback_list')
+    return render(request, 'shipping/feedback_confirm_delete.html', {'feedback': feedback})
+
